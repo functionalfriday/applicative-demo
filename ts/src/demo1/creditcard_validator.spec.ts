5edacf7a73759cb1e4b725bb0fce564d5ca03a3b
Patrick Drechsler <patrick@pdrechsler.de>
2021-06-07 19:58:35 +0200

    ⚗️ trying to figure out how to use fp-ts

--- ts/src/demo1/creditcard_validator.spec.ts
+++ ts/src/demo1/creditcard_validator.spec.ts
@@ -1,4 +1,4 @@
-import { right } from "fp-ts/lib/Either";
+import { left, right } from "fp-ts/lib/Either";
 import { CreditCard, validate } from "./creditcard_validator";
 
 describe("credit card validator", () => {
@@ -14,6 +14,27 @@ describe("credit card validator", () => {
         const result = validate(creditcard);
         
         // assert
-        expect(result).toEqual(right("happy case"));
+        expect(result).toEqual(right(creditcard));
+    })
+
+    it("invalid inputs create correct validation error message", () => {
+        
+        // arrange
+        const number = "invalid";
+        const expiry = "invalid";
+        const cvv = "invalid";
+        const creditcard = new CreditCard(number, expiry, cvv);
+        
+        // act
+        const result = validate(creditcard);
+        
+        // assert
+        const expected = left([
+                "invalid number",
+                "invalid expiry",
+                "invalid cvv",
+            ]);
+
+        expect(result).toEqual(expected);
     })
 })
--- ts/src/demo1/creditcard_validator.ts
+++ ts/src/demo1/creditcard_validator.ts
@@ -1,4 +1,7 @@
-import { Either, right } from "fp-ts/lib/Either";
+// import { Either, right } from "fp-ts/lib/Either";
+import * as E from "fp-ts/lib/Either"
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/function";
 import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray";
 
 export class CreditCard {
@@ -13,6 +16,58 @@ export class CreditCard {
     }
 }
 
-export const validate = (card: CreditCard): Either<NonEmptyArray<string>, string> => {
-    return right("happy case");
+export const validate = (card: CreditCard): E.Either<NonEmptyArray<string>, CreditCard> => {
+    // return right("happy case");
+
+    const f = (a:string) => (b:string) => (c:string) =>
+        new CreditCard(a, b, c);
+
+    // Demo from fp-ts docs ---------------------------------------------------
+    // const fa: O.Option<string> = O.some('a');
+    // const fb: O.Option<string> = O.some('b');
+    // const fc: O.Option<string> = O.some('c');
+
+    // const resultDemo = pipe(
+    //     O.some(f),
+    //     O.ap(fa),
+    //     O.ap(fb),
+    //     O.ap(fc)
+    // )
+
+    // Using Either instead of Option -----------------------------------------
+    // const fa: E.Either<NonEmptyArray<string>, string> = E.right('a');
+    // const fb: E.Either<NonEmptyArray<string>, string> = E.right('b');
+    // const fc: E.Either<NonEmptyArray<string>, string> = E.right('c');
+
+    // const result = pipe(
+    //     E.right(f),
+    //     E.ap(fa),
+    //     E.ap(fb),
+    //     E.ap(fc)
+    // )
+    // return result;
+
+    // Me, with no idea about TS, trying to port the above --------------------
+    // Of course, it doesn't work...
+    //
+    // const validateNumber: E.Either<NonEmptyArray<string>, string> = (s: string) => {
+    //     return E.right(s);
+    //     return s === "invalid" ? E.left(["invalid number"]) : E.right('a');
+    // }
+    // const validateExpiry: E.Either<NonEmptyArray<string>, string> = (s: string) => {
+    //     return s === "invalid" ? E.left(["invalid expiry"]) : E.right('a');
+    // }
+    // const validateCvv: E.Either<NonEmptyArray<string>, string> = (s: string) => {
+    //     return s === "invalid" ? E.left(["invalid cvv"]) : E.right('a');
+    // }
+    
+    // const result2 = pipe(
+    //     E.right(f),
+    //     E.ap(validateNumber),
+    //     E.ap(validateExpiry),
+    //     E.ap(validateCvv)
+    // )
+    // return result;
+
+    return E.right(card);
 }
