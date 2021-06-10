d1e401da488acd6e7354cbfa09fba39a630aa73d
Martin Grotz <martin.grotz@gmx.de>
2021-06-10 20:33:40 +0000

    works with ReadonlyArray concat

--- ts/src/demo1/creditcard_validator.ts
+++ ts/src/demo1/creditcard_validator.ts
@@ -5,6 +5,8 @@ import * as S from "fp-ts/lib/Semigroup";
 import { pipe } from "fp-ts/lib/function";
 import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray";
 import { Semigroup } from "fp-ts/lib/string";
+// import { getSemigroup, of } from "fp-ts/lib/ReadonlyArray";
+import * as RA from "fp-ts/lib/ReadonlyArray";
 
 export class CreditCard {
     constructor(
@@ -41,102 +43,30 @@ function x() {
 
 export const validate = (
     card: CreditCard
-): E.Either<string, CreditCard> => {
-    // return right("happy case");
-
-    const f = (a: string) => (b: string) => (c: string) =>
+): E.Either<ReadonlyArray<string>, CreditCard> => {
+    const createCreditCard = (a: string) => (b: string) => (c: string) =>
         new CreditCard(a, b, c);
 
-    // Demo from fp-ts docs ---------------------------------------------------
-    // const fa: O.Option<string> = O.some('a');
-    // const fb: O.Option<string> = O.some('b');
-    // const fc: O.Option<string> = O.some('c');
 
-    // const resultDemo = pipe(
-    //     O.some(f),
-    //     O.ap(fa),
-    //     O.ap(fb),
-    //     O.ap(fc)
-    // )
-
-    // Using Either instead of Option -----------------------------------------
-    // const fa: E.Either<NonEmptyArray<string>, string> = E.right('a');
-    // const fb: E.Either<NonEmptyArray<string>, string> = E.right('b');
-    // const fc: E.Either<NonEmptyArray<string>, string> = E.right('c');
-
-    // const result = pipe(
-    //     E.right(f),
-    //     E.ap(fa),
-    //     E.ap(fb),
-    //     E.ap(fc)
-    // )
-    // return result;
-
-    // Me, with no idea about TS, trying to port the above --------------------
-    // Of course, it doesn't work...
-    //
-    // const validateNumber: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return E.right(s);
-    //     return s === "invalid" ? E.left(["invalid number"]) : E.right('a');
-    // }
-    // const validateExpiry: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return s === "invalid" ? E.left(["invalid expiry"]) : E.right('a');
-    // }
-    // const validateCvv: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return s === "invalid" ? E.left(["invalid cvv"]) : E.right('a');
-    // }
-
-    const v1 = (s: string): E.Either<string, string> => {
-        return s !== "invalid" ? E.right(s) : E.left("invalid number");
+    const v1 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+        return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid number"));
     };
 
-    const v2 = (s: string): E.Either<string, string> => {
-        return s !== "invalid" ? E.right(s) : E.left("invalid expiry");
+    const v2 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+        return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid expiry"));
     };
 
-    const v3 = (s: string): E.Either<string, string> => {
-        return s !== "invalid" ? E.right(s) : E.left("invalid cvv");
+    const v3 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+        return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid cvv"));
     };
 
 
-    const V = E.getApplicativeValidation((Semigroup));
+    const V = E.getApplicativeValidation((RA.getSemigroup<string>()));
 
-    const liftedFunction = V.of(f);
+    const liftedFunction = V.of(createCreditCard);
     const afterFirstValidation = V.ap(liftedFunction, v1(card.number));
     const afterSecondValidation = V.ap(afterFirstValidation, v2(card.expiry));
     const afterThirdValidation = V.ap(afterSecondValidation, v3(card.cvv));
 
     return afterThirdValidation;
-    // const r2 = pipe(
-    //     ,
-    //     x.ap(v1(card.number)),
-    //     x.ap(v2(card.expiry)),
-    //     x.ap(v3(card.cvv)),
-    // );
-
-
-
-
-
-
-
-
-
-    const r = pipe(
-        E.of(f),
-        E.ap(v1(card.number)),
-        E.ap(v2(card.expiry)),
-        E.ap(v3(card.cvv))
-    );
-
-    return r;
-    // const result2 = pipe(
-    //     E.right(f),
-    //     E.ap(validateNumber),
-    //     E.ap(validateExpiry),
-    //     E.ap(validateCvv)
-    // )
-    // return result;
-
-    //   return E.right(card);
 };
