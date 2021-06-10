12998b36a8b4a28123757f2737aa41241e4f79c3
Martin Grotz <martin.grotz@gmx.de>
2021-06-10 07:45:46 +0200

    try to get typescript apply working - not collecting errors yet

--- ts/src/demo1/creditcard_validator.spec.ts
+++ ts/src/demo1/creditcard_validator.spec.ts
@@ -2,39 +2,33 @@ import { left, right } from "fp-ts/lib/Either";
 import { CreditCard, validate } from "./creditcard_validator";
 
 describe("credit card validator", () => {
-    it("valid inputs create valid credit card", () => {
-        
-        // arrange
-        const number = "123";
-        const expiry = "345";
-        const cvv = "678";
-        const creditcard = new CreditCard(number, expiry, cvv);
-        
-        // act
-        const result = validate(creditcard);
-        
-        // assert
-        expect(result).toEqual(right(creditcard));
-    })
+  it("valid inputs create valid credit card", () => {
+    // arrange
+    const number = "123";
+    const expiry = "345";
+    const cvv = "678";
+    const creditcard = new CreditCard(number, expiry, cvv);
 
-    it("invalid inputs create correct validation error message", () => {
-        
-        // arrange
-        const number = "invalid";
-        const expiry = "invalid";
-        const cvv = "invalid";
-        const creditcard = new CreditCard(number, expiry, cvv);
-        
-        // act
-        const result = validate(creditcard);
-        
-        // assert
-        const expected = left([
-                "invalid number",
-                "invalid expiry",
-                "invalid cvv",
-            ]);
+    // act
+    const result = validate(creditcard);
 
-        expect(result).toEqual(expected);
-    })
-})
+    // assert
+    expect(result).toEqual(right(creditcard));
+  });
+
+  it("invalid inputs create correct validation error message", () => {
+    // arrange
+    const number = "invalid";
+    const expiry = "invalid";
+    const cvv = "invalid";
+    const creditcard = new CreditCard(number, expiry, cvv);
+
+    // act
+    const result = validate(creditcard);
+
+    // assert
+    const expected = left(["invalid number", "invalid expiry", "invalid cvv"]);
+
+    expect(result).toEqual(expected);
+  });
+});
--- ts/src/demo1/creditcard_validator.ts
+++ ts/src/demo1/creditcard_validator.ts
@@ -1,73 +1,116 @@
 // import { Either, right } from "fp-ts/lib/Either";
-import * as E from "fp-ts/lib/Either"
+import * as E from "fp-ts/lib/Either";
 import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/function";
 import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray";
 
 export class CreditCard {
-    number: string;
-    expiry: string;
-    cvv: string;
-    
-    constructor(number: string, expiry: string, cvv: string) {
-        this.number = number;
-        this.expiry = expiry;
-        this.cvv = cvv;
-    }
+  constructor(
+    public readonly number: string,
+    public readonly expiry: string,
+    public readonly cvv: string
+  ) {}
 }
 
-export const validate = (card: CreditCard): E.Either<NonEmptyArray<string>, CreditCard> => {
-    // return right("happy case");
+function x() {
+  const n1 = O.of(3);
+  const n2 = O.of(4);
+  const n3 = O.of(5);
 
-    const f = (a:string) => (b:string) => (c:string) =>
-        new CreditCard(a, b, c);
+  const f =
+    (num1: number) =>
+    (num2: number) =>
+    (num3: number): number =>
+      num1 * num2 * num3;
 
-    // Demo from fp-ts docs ---------------------------------------------------
-    // const fa: O.Option<string> = O.some('a');
-    // const fb: O.Option<string> = O.some('b');
-    // const fc: O.Option<string> = O.some('c');
+  //   const f1 = O.ap(n1);
+  //   const f2 = f1(O.of(f));
 
-    // const resultDemo = pipe(
-    //     O.some(f),
-    //     O.ap(fa),
-    //     O.ap(fb),
-    //     O.ap(fc)
-    // )
+  const af = O.of(f);
 
-    // Using Either instead of Option -----------------------------------------
-    // const fa: E.Either<NonEmptyArray<string>, string> = E.right('a');
-    // const fb: E.Either<NonEmptyArray<string>, string> = E.right('b');
-    // const fc: E.Either<NonEmptyArray<string>, string> = E.right('c');
+  const af1 = O.ap(n1)(af);
+  const af2 = O.ap(n2)(af1);
+  const af3 = O.ap(n3)(af2);
 
-    // const result = pipe(
-    //     E.right(f),
-    //     E.ap(fa),
-    //     E.ap(fb),
-    //     E.ap(fc)
-    // )
-    // return result;
+  pipe(O.of(f), O.ap(n1), O.ap(n2), O.ap(n3));
 
-    // Me, with no idea about TS, trying to port the above --------------------
-    // Of course, it doesn't work...
-    //
-    // const validateNumber: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return E.right(s);
-    //     return s === "invalid" ? E.left(["invalid number"]) : E.right('a');
-    // }
-    // const validateExpiry: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return s === "invalid" ? E.left(["invalid expiry"]) : E.right('a');
-    // }
-    // const validateCvv: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return s === "invalid" ? E.left(["invalid cvv"]) : E.right('a');
-    // }
-    
-    // const result2 = pipe(
-    //     E.right(f),
-    //     E.ap(validateNumber),
-    //     E.ap(validateExpiry),
-    //     E.ap(validateCvv)
-    // )
-    // return result;
-
-    return E.right(card);
+  return af3;
 }
+
+export const validate = (
+  card: CreditCard
+): E.Either<Array<string>, CreditCard> => {
+  // return right("happy case");
+
+  const f = (a: string) => (b: string) => (c: string) =>
+    new CreditCard(a, b, c);
+
+  // Demo from fp-ts docs ---------------------------------------------------
+  // const fa: O.Option<string> = O.some('a');
+  // const fb: O.Option<string> = O.some('b');
+  // const fc: O.Option<string> = O.some('c');
+
+  // const resultDemo = pipe(
+  //     O.some(f),
+  //     O.ap(fa),
+  //     O.ap(fb),
+  //     O.ap(fc)
+  // )
+
+  // Using Either instead of Option -----------------------------------------
+  // const fa: E.Either<NonEmptyArray<string>, string> = E.right('a');
+  // const fb: E.Either<NonEmptyArray<string>, string> = E.right('b');
+  // const fc: E.Either<NonEmptyArray<string>, string> = E.right('c');
+
+  // const result = pipe(
+  //     E.right(f),
+  //     E.ap(fa),
+  //     E.ap(fb),
+  //     E.ap(fc)
+  // )
+  // return result;
+
+  // Me, with no idea about TS, trying to port the above --------------------
+  // Of course, it doesn't work...
+  //
+  // const validateNumber: E.Either<NonEmptyArray<string>, string> = (s: string) => {
+  //     return E.right(s);
+  //     return s === "invalid" ? E.left(["invalid number"]) : E.right('a');
+  // }
+  // const validateExpiry: E.Either<NonEmptyArray<string>, string> = (s: string) => {
+  //     return s === "invalid" ? E.left(["invalid expiry"]) : E.right('a');
+  // }
+  // const validateCvv: E.Either<NonEmptyArray<string>, string> = (s: string) => {
+  //     return s === "invalid" ? E.left(["invalid cvv"]) : E.right('a');
+  // }
+
+  const v1 = (s: string): E.Either<string[], string> => {
+    return s !== "invalid" ? E.right(s) : E.left(["invalid number"]);
+  };
+
+  const v2 = (s: string): E.Either<string[], string> => {
+    return s !== "invalid" ? E.right(s) : E.left(["invalid expiry"]);
+  };
+
+  const v3 = (s: string): E.Either<string[], string> => {
+    return s !== "invalid" ? E.right(s) : E.left(["invalid cvv"]);
+  };
+
+  const r = pipe(
+    E.of(f),
+    E.ap(v1(card.number)),
+    E.ap(v2(card.expiry)),
+    E.ap(v3(card.cvv))
+  );
+
+  return r;
+  // const result2 = pipe(
+  //     E.right(f),
+  //     E.ap(validateNumber),
+  //     E.ap(validateExpiry),
+  //     E.ap(validateCvv)
+  // )
+  // return result;
+
+  //   return E.right(card);
+};
