e194b697672168088af31534d0c8f3154957333a
Patrick Drechsler <patrick@pdrechsler.de>
2021-06-12 17:37:45 +0200

    ⚗️ ts: another expirement...

--- ts/src/demo1/creditcard_validator.ts
+++ ts/src/demo1/creditcard_validator.ts
@@ -1,8 +1,7 @@
 // import { Either, right } from "fp-ts/lib/Either";
 import * as E from "fp-ts/lib/Either"
-import * as O from "fp-ts/lib/Option";
+import * as RA from "fp-ts/lib/ReadonlyArray";
 import { pipe } from "fp-ts/lib/function";
-import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray";
 
 export class CreditCard {
     number: string;
@@ -16,58 +15,40 @@ export class CreditCard {
     }
 }
 
-export const validate = (card: CreditCard): E.Either<NonEmptyArray<string>, CreditCard> => {
-    // return right("happy case");
+// from https://stackoverflow.com/q/67932588/1062607
+export const validate = (card: CreditCard): E.Either<ReadonlyArray<string>, CreditCard> => {
 
-    const f = (a:string) => (b:string) => (c:string) =>
+    const createCreditCard = (a:string) => (b:string) => (c:string) =>
         new CreditCard(a, b, c);
 
-    // Demo from fp-ts docs ---------------------------------------------------
-    // const fa: O.Option<string> = O.some('a');
-    // const fb: O.Option<string> = O.some('b');
-    // const fc: O.Option<string> = O.some('c');
-
-    // const resultDemo = pipe(
-    //     O.some(f),
-    //     O.ap(fa),
-    //     O.ap(fb),
-    //     O.ap(fc)
-    // )
-
-    // Using Either instead of Option -----------------------------------------
-    // const fa: E.Either<NonEmptyArray<string>, string> = E.right('a');
-    // const fb: E.Either<NonEmptyArray<string>, string> = E.right('b');
-    // const fc: E.Either<NonEmptyArray<string>, string> = E.right('c');
-
-    // const result = pipe(
-    //     E.right(f),
-    //     E.ap(fa),
-    //     E.ap(fb),
-    //     E.ap(fc)
-    // )
-    // return result;
-
-    // Me, with no idea about TS, trying to port the above --------------------
-    // Of course, it doesn't work...
-    //
-    // const validateNumber: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return E.right(s);
-    //     return s === "invalid" ? E.left(["invalid number"]) : E.right('a');
-    // }
-    // const validateExpiry: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return s === "invalid" ? E.left(["invalid expiry"]) : E.right('a');
-    // }
-    // const validateCvv: E.Either<NonEmptyArray<string>, string> = (s: string) => {
-    //     return s === "invalid" ? E.left(["invalid cvv"]) : E.right('a');
-    // }
+        const v1 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+            return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid number"));
+        };
     
-    // const result2 = pipe(
-    //     E.right(f),
-    //     E.ap(validateNumber),
-    //     E.ap(validateExpiry),
-    //     E.ap(validateCvv)
-    // )
-    // return result;
-
-    return E.right(card);
+        const v2 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+            return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid expiry"));
+        };
+    
+        const v3 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+            return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid cvv"));
+        };
+    
+        const V = E.getApplicativeValidation((RA.getSemigroup<string>()));
+    
+        // this does not work, because V.ap wants 2 arguments but only has 1?
+        // const fromPipe = pipe(
+        //     V.of(createCreditCard),
+        //     V.ap(v1(card.number)),
+        //     V.ap(v2(card.expiry)),
+        //     V.ap(v3(card.cvv))
+        // );
+        // return fromPipe;
+    
+        // this works, but is ugly
+        const liftedFunction = V.of(createCreditCard);
+        const afterFirstValidation = V.ap(liftedFunction, v1(card.number));
+        const afterSecondValidation = V.ap(afterFirstValidation, v2(card.expiry));
+        const afterThirdValidation = V.ap(afterSecondValidation, v3(card.cvv));
+    
+        return afterThirdValidation;
 }
