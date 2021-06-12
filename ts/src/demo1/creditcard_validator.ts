9604bbe51ae8689ed61135c389f5fd4f06eaacdb
Patrick Drechsler <patrick@pdrechsler.de>
2021-06-12 17:44:33 +0200

    ✨ ts solution works

--- ts/src/demo1/creditcard_validator.ts
+++ ts/src/demo1/creditcard_validator.ts
@@ -2,6 +2,7 @@
 import * as E from "fp-ts/lib/Either"
 import * as RA from "fp-ts/lib/ReadonlyArray";
 import { pipe } from "fp-ts/lib/function";
+import { pipeable } from "fp-ts/lib/pipeable";
 
 export class CreditCard {
     number: string;
@@ -15,40 +16,33 @@ export class CreditCard {
     }
 }
 
-// from https://stackoverflow.com/q/67932588/1062607
+const createCreditCard = (a:string) => (b:string) => (c:string) =>
+    new CreditCard(a, b, c);
+
+const v1 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+    return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid number"));
+};
+
+const v2 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+    return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid expiry"));
+};
+
+const v3 = (s: string): E.Either<ReadonlyArray<string>, string> => {
+    return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid cvv"));
+};
+
+
+// from https://stackoverflow.com/a/67940028/1062607
 export const validate = (card: CreditCard): E.Either<ReadonlyArray<string>, CreditCard> => {
-
-    const createCreditCard = (a:string) => (b:string) => (c:string) =>
-        new CreditCard(a, b, c);
-
-        const v1 = (s: string): E.Either<ReadonlyArray<string>, string> => {
-            return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid number"));
-        };
+    const validation = E.getApplicativeValidation(RA.getSemigroup<string>())
+    const V = pipeable(validation)
     
-        const v2 = (s: string): E.Either<ReadonlyArray<string>, string> => {
-            return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid expiry"));
-        };
+    const fromPipe = pipe(
+      validation.of(createCreditCard),
+      V.ap(v1(card.number)),
+      V.ap(v2(card.expiry)),
+      V.ap(v3(card.cvv))
+    )
     
-        const v3 = (s: string): E.Either<ReadonlyArray<string>, string> => {
-            return s !== "invalid" ? E.right(s) : E.left(RA.of("invalid cvv"));
-        };
-    
-        const V = E.getApplicativeValidation((RA.getSemigroup<string>()));
-    
-        // this does not work, because V.ap wants 2 arguments but only has 1?
-        // const fromPipe = pipe(
-        //     V.of(createCreditCard),
-        //     V.ap(v1(card.number)),
-        //     V.ap(v2(card.expiry)),
-        //     V.ap(v3(card.cvv))
-        // );
-        // return fromPipe;
-    
-        // this works, but is ugly
-        const liftedFunction = V.of(createCreditCard);
-        const afterFirstValidation = V.ap(liftedFunction, v1(card.number));
-        const afterSecondValidation = V.ap(afterFirstValidation, v2(card.expiry));
-        const afterThirdValidation = V.ap(afterSecondValidation, v3(card.cvv));
-    
-        return afterThirdValidation;
+    return fromPipe;
 }
